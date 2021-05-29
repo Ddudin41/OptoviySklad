b3cd3ca12f02ec3657ca4c8ee5ebad09e9b1e1bb
Dprrrr <dprrrr1987@mail.ru>
2021-05-29 11:22:08 +0300

    first

---
+++ OptSklad/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OptSklad
+{
+    public partial class Product
+    {
+        public Uri ImagePreview
+        {
+            get
+            {
+                var ImageName = Environment.CurrentDirectory + Image ?? "";
+                return System.IO.File.Exists(ImageName) ? new Uri(ImageName) : new Uri("pack://application:,,,/Images/picture.png");
+            }
+        }
+        public string Sklad
+        {
+            get
+            {
+                var res = "";
+                //res = "Склад № ";
+                foreach (var ps in ProductSklad)
+                {
+                    res = "Склад № " + ps.SkladID + ", " + ps.Sklad.Адрес + ", В количестве: " + ps.Count;
+                }
+                if(res=="")
+                {
+                    res = "Нет на складах";
+                }
+                return res;
+            }
+        }
+    }
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private IEnumerable<Product> _ProductList;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void Invalidate()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ProductList"));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("CurrentPage"));
+        }
+        private int _CurrentPage = 1;
+
+        public int CurrentPage
+        {
+            get
+            {
+                return _CurrentPage;
+            }
+            set
+            {
+                // тут проверка, чтобы номер не уходил в минус и в плюс
+                if (value > 0)
+                {
+                    if ((_ProductList.Count() % 4)==0)
+                    {
+                        if (value <= _ProductList.Count() / 4)
+                        {
+                            _CurrentPage = value;
+                            Invalidate();
+                        }
+                    } else
+                    {
+                        if (value <= (_ProductList.Count() / 4)+1)
+                        {
+                            _CurrentPage = value;
+                            Invalidate();
+                        }
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Product> ProductList
+        {
+            get
+            {
+                var Result = _ProductList;
+
+                switch (SortType)
+                {
+                    case 1:
+                        Result = Result.OrderBy(p => p.Title);
+                        break;
+                    case 2:
+                        Result = Result.OrderByDescending(p => p.Title);
+                        break;
+                    case 3:
+                        Result = Result.OrderBy(p => p.Cost);
+                        break;
+                    case 4:
+                        Result = Result.OrderByDescending(p => p.Cost);
+                        break;
+                }
+
+                return Result.Skip((CurrentPage - 1) * 4).Take(4);
+            }
+            set
+            {
+                _ProductList = value;
+            }
+        }
+        private int SortType = 0;
+        public string[] SortList { get; set; } = {
+            "Без сортировки",
+            "название по убыванию",
+            "название по возрастанию",
+            "цена по убыванию",
+            "цена по возрастанию" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ProductList = Core.DB.Product.ToArray();
+        }
+        private void PrevPage_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage--;
+        }
+
+        private void NextPage_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentPage++;
+        }
+        private void SortTypeComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SortType = SortTypeComboBox.SelectedIndex;
+            Invalidate();
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            var AddWindow = new ProductWindow(new Product(),0);
+            if (AddWindow.ShowDialog() == true)
+            {
+                ProductList = Core.DB.Product.ToArray();
+                Invalidate();
+            }
+        }
+
+        private void ProductListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var EditWindow = new ProductWindow(ProductListView.SelectedItem as Product,1);
+            if (EditWindow.ShowDialog() == true)
+            {
+                ProductList = Core.DB.Product.ToArray();
+                Invalidate();
+            }
+        }
+    }
+}
---
+++ OptSklad/ProductWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OptSklad
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductWindow.xaml
+    /// </summary>
+    public partial class ProductWindow : Window
+    {
+        public Product CurrentProduct { get; set; }
+        public IEnumerable<ProductType> ProdTypeList { get; set; }
+        public IEnumerable<ProductSklad> SkladList { get; set; }
+        public Sklad CurSklad;
+        public ProductWindow(Product CurProduct,int type)
+        {
+            InitializeComponent();
+            DataContext = this;
+            CurrentProduct = CurProduct;
+            ProdTypeList = Core.DB.ProductType.ToArray();
+            SkladList = Core.DB.ProductSklad.ToArray();
+            if (type == 0)
+            {
+                DeleteButton.Visibility = Visibility.Collapsed;
+            } else
+            {
+                DeleteButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (CurrentProduct.ProductType == null)
+                    throw new Exception("Не выбран Тип продукта");
+                if (CurrentProduct.Title == "")
+                    throw new Exception("Не выбрано Наименование");
+                if (CurrentProduct.ArticleNumber == null)
+                    throw new Exception("Не указан артикул");
+                if (CurrentProduct.Cost == 0)
+                    throw new Exception("Не указан цена");
+                if (CurrentProduct.ID == 0)
+                    Core.DB.Product.Add(CurrentProduct);
+                Core.DB.SaveChanges();
+                DialogResult = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentProduct.ProductSklad.Count > 0)
+            {
+                MessageBox.Show("Нельзя удалить товар, который лежит на складе");
+                return;
+            }
+            try
+            {
+                Core.DB.Product.Remove(CurrentProduct);
+                Core.DB.SaveChanges();
+                DialogResult = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при удалении товара: {ex.Message}");
+            }
+        }
+    }
+}
